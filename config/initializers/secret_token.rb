e7b55594e5c3bc7764c2fa00f558e8bb06430794
Mr. Matt <system9@composition9.com>
2013-10-12 16:46:37 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SinglePageQr::Application.config.secret_key_base = '88a7863ec1718f68c13ea0d4d35622e8ceb012c101d41f650bbe15fdabced2b8fdc8c58099182bb343bf3f14f9df40e9e8445d07505ac8bd9cfdd7fd9bc32ab7'
